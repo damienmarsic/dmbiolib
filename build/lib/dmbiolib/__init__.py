24975d7881ddf7e326c066033e49091a7aa036ea
Damien Marsic(Portonbio) <Damien.Marsic@portonbio.com>
2022-06-14 14:43:24 +0800

    0.2.5

--- build/lib/dmbiolib/__init__.py
+++ build/lib/dmbiolib/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-__version__='0.1.0'
-last_update='2022-04-11'
+__version__='0.2.5'
+last_update='2022-06-09'
 author='Damien Marsic, damien.marsic@aliyun.com'
 
-import sys,glob,os,gzip,time,math
+import sys,glob,os,gzip,time,math,argparse
+import numpy as np
 
 ambiguous="ryswkmbdhvn"
 aa="ARNDCQEGHILKMFPSTWYV"
@@ -21,67 +22,56 @@ def check_file(filename,strict):
     else:
         return True
 
-def check_format(x):
+def check_plot_format(x):
     format=''
-    if x not in ('Single multipage pdf','svg','png','jpg','jpeg','pdf','ps','eps','pgf','raw','rgba','tif','tiff'):
-        print("\n  File format not recognized. Options are svg, png, jpg, pdf, ps, eps, pgf, raw, rgba, tif, tiff (or single multipage pdf file with no -f argument).\n")
+    y=('svg','png','jpg','jpeg','pdf','ps','eps','pgf','raw','rgba','tif','tiff')
+    if x not in ('Single multipage pdf',)+y:
+        print("\n  File format not recognized. Options are: "+', '.join(y)+" (or single multipage pdf file with no -f argument).\n")
         sys.exit()
     if len(x)<5:
         format=x
     return format
 
-def check_seq(filename,type,required,strict,limit):
-    if filename[-3:]=='.gz':
-        f=gzip.open(filename,'rt')
+def check_read_file(x):
+    fail=''
+    if check_file(x,False):
+        f=open_read_file(x)
+        for i in range(5):
+            y=f.readline().strip()
+            if y:
+                break
+        if y and y[0] in ('@','>'):
+            y=f.readline().strip().lower()
+            t,req=check_seq(y,'atgc'+ambiguous,'atgc')
+            if not t:
+                fail+='\n  File '+x+' contains invalid characters!'
+            if not req:
+                fail+='\n  File '+x+' does not contain expected characters!'
+        else:
+            fail+='\n  File '+x+' has incorrect format!'
+        f.close()
     else:
-        f=open(filename,'r')
+        fail+='\n  File '+x+' not found!'
+    return fail
+
+def check_seq(seq,type,required):
     t=True
     req=False
-    count=0
-    new=''
-    for line in f:
-        if limit and count==limit+1:
+    for i in seq:
+        if i not in type:
+            t=False
             break
-        if type==aa:
-            l=line.upper().strip()
-        else:
-            l=line.lower().strip()
-        if l and l[0] in ('>','@'):
-            count+=1
-            new=l[0]
-            continue
-        if not new:
-            continue
-        if new=='@':
-            new=''
-        for i in l:
-            if i not in type and not i.isdigit() and i!=' ':
-                t=False
-                break
-            if i in required:
-                req=True
-        if t==False:
-            if strict:
-                break
-            f.close()
-            return False
-    else:
-        f.close()
-        if req==True:
-            return True
-        if not strict and count>0:
-            return False
-        if count==0:
-            print("\n  File "+filename+" does not contain any fasta or fastq sequence!\n")
-            sys.exit()
-    f.close()
-    if limit and count==limit+1:
-        return True
-    if t==False:
-        print("\n  File "+filename+" contains invalid characters!\n")
-    elif req==False:
-        print("\n  File "+filename+" does not contain any '"+required+"' nucleotide!\n")
-    sys.exit()
+        if i in required:
+            req=True
+    return t,req
+
+def check_sync(l1,l2):
+    fail=''
+    if l1[0] not in ('@','>') or l2[0] not in ('@','>'):
+        fail='\n  Wrong read file format! Only fastq, gzipped fastq or fasta are accepted formats!'
+    elif l1[:l1.find(' ')]!=l2[:l2.find(' ')]:
+        fail='\n  R1 and R2 reads are not synchronized! Please use raw read files!'
+    return fail
 
 def compress(seq):
     x=''
@@ -102,20 +92,6 @@ def diff(seqs):
         z=min(z,y)
     return z
 
-def endplot(fig,counter,x,format,mppdf):
-    plt.legend()
-    fig.subplots_adjust(bottom=0.15)
-    fig.tight_layout()
-    if format:
-        counter+=1
-        g=x+str(counter)+'.'+format
-        plt.savefig(g,dpi=600)
-        print('  Figure was saved into file: '+g+'\n')
-    else:
-        mppdf.savefig()
-    plt.close()
-    return counter
-
 def entropy(matrix):
     score=0
     for n in matrix:
@@ -127,71 +103,77 @@ def entropy(matrix):
         score+=H
     return score
 
-def findreadfiles(pattern,seqtype,exclude):
-    if pattern=='auto':
-        x='*.f*'
-    elif check_file(pattern,False):
-        x=pattern
-    else:
-        x=pattern+'*'
-    rfiles=glob.glob(x)
-    if len(rfiles)>1:
-        rfiles=[x for x in rfiles if (len(x)>5 and x[-5:] in ('fasta','fastq','fa.gz','fq.gz')) or (len(x)>3 and x[-3:] in ('.fa','.fq')) or (len(x)>8 and x[-8:] in ('fasta.gz','fastq.gz'))]
-    rfiles=[x for x in rfiles if check_seq(x,seqtype[0],seqtype[1],False,5)]
-    if not rfiles:
-        print("\n  No read file found. Check pattern, or move to correct directory, or include path in pattern.\n")
-        sys.exit()
-    for n in exclude:
-        if n and n in rfiles:
-            rfiles.remove(n)
-    rfiles=dict.fromkeys(rfiles,0)
-    for rfile in rfiles:
-        if rfile[-3:]=='.gz':
-            f=gzip.open(rfile,'r')
-        elif rfile[-1]=='q':
-            f=open(rfile,'rb')
-        else:
-            f=open(rfile,'r')
-        if rfile[-3:]=='.gz' or rfile[-1]=='q':
-            nr=lncount(f)//4
-        else:
-            nr=f.read().count('>')
-        f.close()
-        rfiles[rfile]=nr
-    print('  Read file(s) (number of reads):')
-    for n in rfiles:
-        print('  '+n+' ('+str(rfiles[n])+')')
-    print()
-    return rfiles
+def fsize(filename):
+    return os.path.getsize(filename)
 
-def getread(f,y,z,counter):
+def getfasta(fn,type,required,multi):
+    fail=''
+    seq={}
+    if not check_file(fn,False):
+        fail+='\n  File '+fn+' could not be found!'
+        return seq,fail
+    with open(fn,'r') as f:
+        x=f.read().strip()
+    if x[0]!='>':
+        fail+='\n  '+fn+' is not a fasta file!'
+        return seq,fail
+    if not multi and x.count('>')>1:
+        fail+='\n  File '+fn+' contains more than one sequence!'
+        return seq,fail
+    x=x.split('>')
+    for n in x:
+        if not n:
+            continue
+        y=n[:n.find('\n')]
+        y=y[:max(len(y),y.find(' '))]
+        if not y and multi:
+            fail+='\n  Sequence with no name found in file '+fn+'! All sequences must have a name!'
+        z=n[n.find('\n'):]
+        if y in seq:
+            fail+='\n  Duplicate sequence name '+y+' found in file '+fn+'! All sequences must have a different name!'
+        z=z.replace(' ','').replace('\n','')
+        if type==aa:
+            z=z.upper()
+        else:
+            z=z.lower()
+        t,req=check_seq(z,type,required)
+        if not t:
+            fail+='\n  Sequence '+y+' in '+fn+' contains invalid characters!'
+        if not req:
+            fail+='\n  Sequence '+y+' in '+fn+' does not contain expected characters!'
+        if y and not fail:
+            seq[y]=z
+    return seq,fail
+
+def getread(f,y,counter):
+    name=''
     if not y:
         l=''
         while True:
             line=f.readline().strip()
             if not line:
                 line=f.readline().strip()
+            if line[0]=='>':
+                name=line
             if not line or (l and line[0]=='>'):
                 break
             if not l and line[0]=='>':
                 continue
             l+=line
     else:
+        z=0
         while z<y:
             l=f.readline().strip()
+            if not z:
+                name=l
             z+=1
     if not l:
-        return l,f,z,counter
-    l=l.lower()
-    z=0
+        return l,f,counter,name
     counter+=1
-    return l,f,z,counter
+    return l.lower(),f,counter,name
 
 def initreadfile(rfile):
-    if rfile[-3:]=='.gz':
-        f=gzip.open(rfile,'rt')
-    else:
-        f=open(rfile,'r')
+    f=open_read_file(rfile)
     l=f.readline().strip()
     if not l or l[0] not in ('>','@'):
         f.close()
@@ -207,9 +189,8 @@ def initreadfile(rfile):
     else:
         y=4
     f.seek(0)
-    z=y-2
     counter=0
-    return f,y,z,counter
+    return f,y,counter
 
 def lncount(f):
     def _make_gen(reader):
@@ -237,6 +218,13 @@ def nt_match(nt1, nt2):
     else:
         return False
 
+def open_read_file(x):
+    if x[-2:]=='gz':
+        f=gzip.open(x,'rt')
+    else:
+        f=open(x,'r')
+    return f
+
 def override(func):
     class OverrideAction(argparse.Action):
         def __call__(self, parser, namespace, values, option_string):
@@ -244,63 +232,62 @@ def override(func):
             parser.exit()
     return OverrideAction
 
+def plot_end(fig,counter,x,format,mppdf):
+    plt.legend()
+    fig.subplots_adjust(bottom=0.15)
+    fig.tight_layout()
+    if format:
+        counter+=1
+        g=x+str(counter)+'.'+format
+        plt.savefig(g,dpi=600)
+        print('  Figure was saved into file: '+g+'\n')
+    else:
+        mppdf.savefig()
+    plt.close()
+    return counter
+
+def plot_start(x,y):
+    colors=plt.get_cmap(x)
+    fig=plt.figure(figsize=(12,6.75))
+    plt.title(y,size=15,weight='roman')
+    return colors,fig
+
 def pr2(f,t):
     print(t)
     f.write(t+'\n')
 
-def readfasta(filename,type):
-    f=open(filename,'r')
-    seq=1
-    for line in f:
-        if line and line[0]=='>':
-            if seq!=1:
-                print('\n  File '+filename+' contains more than one sequence !\n')
-                sys.exit()
-            seq=''
-            continue
-        if seq!=1:
-            if type==aa:
-                l=line.upper().strip()
-            else:
-                l=line.lower().strip()
-            l=''.join(x for x in l if not x.isdigit() and x!=' ')
-            seq+=l
-    f.close()
-    if not seq or seq==1:
-        if type==aa:
-            x='Protein'
-        else:
-            x='DNA'
-        print('\n  '+x+' sequence could not be found in '+filename+' !\n')
-        sys.exit()
-    return seq
+def progress_check(c,show,t):
+    if c in show:
+        k=show[c]
+        print('\r  '+t+' '*(4-len(k))+k+'%',end='')
 
-def readmultifasta(filename,type):
-    f=open(filename,'r')
-    temp=f.read().strip()
+def progress_end():
+    print('\b\b\b\b\b\b100.0%\n')
+
+def progress_start(nr,t):
+    y=np.arange(0,nr,nr/1000)
+    x=[round(n) for n in y]
+    z=np.arange(0,100,0.1)
+    y=[str(round(n,1)) for n in z]
+    show=dict(zip(x,y))
+    print('\n  '+t+'     0.0%',end='')
+    return show
+
+def readcount(R,fail):
+    if R[-3:]=='.gz':
+        f=gzip.open(R,'r')
+    elif R[-1]=='q':
+        f=open(R,'rb')
+    else:
+        f=open(R,'r')
+    if R[-3:]=='.gz' or R[-1]=='q':
+        nr=lncount(f)//4
+    else:
+        nr=f.read().count('>')
     f.close()
-    seq={}
-    if not '>' in temp:
-        print('\n  '+filename+' is not a fasta file!\n\n')
-        sys.exit()
-    while temp[0]!='>':
-        temp=temp[1:]
-    temp=temp.split('>')
-    for n in temp:
-        x=n[:n.find('\n')]
-        x=x[:max(len(x),x.find(' '))]
-        y=n[n.find('\n'):]
-        if x in seq:
-            print('\n  Duplicate name '+x+' found in parental sequences! All sequences must have a different name!\n\n')
-            sys.exit()
-        y=y.replace(' ','').replace('\n','')
-        if type==aa:
-            y=y.upper()
-        else:
-            y=y.lower()
-        if x:
-            seq[x]=y
-    return seq
+    if nr<2:
+        fail+='\n  File '+R+' is not a read file!'
+    return(nr,fail)
 
 def rename(name):
     if glob.glob(name):
@@ -309,11 +296,21 @@ def rename(name):
         os.rename(name,n)
         print('\n  Existing '+name+' file was renamed as '+n+'\n  Creating new '+name+' file...\n')
 
-def startplot(x,y):
-    colors=plt.get_cmap(x)
-    fig=plt.figure(figsize=(12,6.75))
-    plt.title(y,size=15,weight='roman')
-    return colors,fig
+def shortest_probe(seqs,lim,t):
+    if lim<1:
+        lim=1
+    fail=''
+    q=-1
+    x=min([len(k) for k in seqs])
+    y=set([k[-x:] for k in seqs])
+    if len(y)!=len(seqs):
+        fail='\n  Duplicate '+t+' found! '+t[-1:].upper()+t[1:]+'s must all be different when trimmed to their maximal common size!'
+    if not fail:
+        q=lim
+        while len(y)!=len(seqs):
+            q+=1
+            y=set([k[-q:] for k in seqs])
+    return q,fail
 
 def version():
     print('\n  Project: '+sys.argv[0][max(sys.argv[0].rfind('\\'),sys.argv[0].rfind('/'))+1:-3]+'\n  version: '+__version__+'\n  Latest update: '+last_update+'\n  Author: '+author+'\n  License: GNU General Public v3 (GPLv3)\n')
