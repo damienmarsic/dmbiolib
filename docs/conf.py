48da5ef2805d1ac09e57bd01cab6b018184abda5
Damien Marsic(Portonbio) <Damien.Marsic@portonbio.com>
2023-01-19 16:32:29 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'stanford_theme'
+html_theme = 'cloud'
 
 html_static_path = ['_static']
 
